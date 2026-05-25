96bd471b7ed2d59dd00018fe3eddfcacf0056b3b
Lawrance Chen <chenlawrance@gmail.com>
2026-05-24 23:22:32 -0700

    solve neetcode: array

--- Neetcode150/Group Anagrams.py
+++ Neetcode150/Group Anagrams.py
@@ -1,3 +1,4 @@
+from typing import List
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
     
--- Neetcode150/Top K Frequent Elements.py
+++ Neetcode150/Top K Frequent Elements.py
@@ -1,3 +1,5 @@
+from typing import List
+
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         from collections import defaultdict
